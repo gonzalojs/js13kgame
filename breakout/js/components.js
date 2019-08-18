2958e5f07421e6a0406fc716b581ccb21a2c2057
gonzalojs <eogutier@gmail.com>
2019-08-18 02:16:04 -0400

    listo

--- breakout/js/components.js
+++ breakout/js/components.js
@@ -1,5 +1,29 @@
 //load images
-
-//load bg images
 const BG_IMG = new Image()
 BG_IMG.src = '../images/bg.jpg'
+
+const LEVEL_IMG = new Image()
+LEVEL_IMG.src = '../images/level.png'
+
+const LIFE_IMG = new Image()
+LIFE_IMG.src = '../images/life.png'
+
+const SCORE_IMG = new Image()
+SCORE_IMG.src = '../images/score.png'
+//end load images
+
+//sound loading
+const WALL_HIT = new Audio()
+WALL_HIT.src = '../sounds/wall.mp3'
+
+const LIFE_LOST = new Audio()
+LIFE_LOST.src = '../sounds/life_lost.mp3'
+
+const PADDLE_HIT = new Audio()
+PADDLE_HIT.src = '../sounds/paddle_hit.mp3'
+
+const WIN = new Audio()
+WIN.src = '../sounds/win.mp3'
+
+const BRICK_HIT = new Audio()
+BRICK_HIT.src = '../sounds/brick_hit.mp3'
